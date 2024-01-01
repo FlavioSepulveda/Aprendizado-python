32db86b13de2fe47392f7148c388561df6151f8f
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-01 17:57:47 -0300

    Correção de erro

--- Python/Dia - 1/modulo01.py
+++ Python/Dia - 1/modulo01.py
@@ -75,4 +75,52 @@ Variavel7 = "Bem vindo à 2024"
 print(variavel7) # Esta retorna 4
 print(Variavel7) # Esta retorna uma frase.
 
-# No modulo 2, Tipos de dados.
+# Existem regras pra nomear uma variavel.
+
+'''
+    OS MANDAMENTOS DAS VARIAVEIS - Estreando variaveis em python:
+        1 # Nomes de variaveis sempre devem começar com letras ou underline;
+        2 # Embora possam conter numeros, nunca nomeie uma variavel com um numero no inicio;
+        3 # Variaveis podem conter dígitos alfanuméricos  (Letras e Números);
+        4 # Os nomes diferenciam de maiúsculas e minusculas;
+'''
+
+# Exemplos -
+minhavariavel = "Flavio" # Todas as letras minusculas
+minha_variavel = "Flavio" # Um underline no meio da varivel
+_minha_variavel = "Flavio" # Iniciando o nome com underline
+minhaVariavel = "Flavio" # Iniciando o nome da variavel com minuscula e colocando letras maiusculas no meio (Camel case)
+MINHAVARIAVEL = "Flavio" # Todas as letras maiusculas
+minhavariavel2 = "Sepulveda" # Numero ao fim do nome
+
+# As formas citadas anteriormente são corretas porem aqui alguns exemplos de erros;
+'''
+    2minhavariavel = "Flavio" (Não pode iniciar com número)
+    minha-variavel = "Flavio" (Traço não é separação)
+    minha variavel = "Flavio" (Não pode ter espaçamento vazio)
+'''
+
+'''
+    Existem varias formas de se escrever nomes de variaveis extensas.
+    Aqui temos alguns exemplos das mais utilizadas:
+    Camel case - A separação das palavras exceto as primeiras, se faz com letra maiusculas;
+        * EX.: minhaVariavelExemplo = "Camel Case"
+    Pascal case - As primeiras letras inclusive da primeira palavra são maiusculas;
+        * EX.: MinhaVariavelExemplo = "Pascal Case"
+    Snake case - A separação das palavras é feita por meio de underline aqui;
+        * EX.: minha_variavel_exemplo = "Snake Case"
+'''
+
+# Atribuição de valores as variavveis
+
+# Python permite atribuição de valores a diferentes variaveis sem a nescessidade de saltar varias linhas;
+'''
+    Em outras linguagens as variaveis devem ser feitas em linhas separadas, assim:
+    var1 = 1
+    var2 = 2
+    var3 = 3
+'''
+
+# Em python esta atribuição pode ser feita de maneira mais simples e em apenas uma linha;
+# POR EXEMPLO -
+var1, var2, var3 = 1, 2, 3 # Esta forma de atribuição é mais simples mas prioriza a ordem a qual você que atribuir alguns valores.
