be9e6cb7b954badcac6bd6c6acdde70b85b3c6fd
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-01 19:13:45 -0300

    Finalizando o modulo 1.

--- Python/Dia - 1/modulo01.py
+++ Python/Dia - 1/modulo01.py
@@ -83,6 +83,7 @@ print(Variavel7) # Esta retorna uma frase.
         2 # Embora possam conter numeros, nunca nomeie uma variavel com um numero no inicio;
         3 # Variaveis podem conter dígitos alfanuméricos  (Letras e Números);
         4 # Os nomes diferenciam de maiúsculas e minusculas;
+        5 # Os nomes não devem conter acentos;
 '''
 
 # Exemplos -
@@ -124,3 +125,126 @@ minhavariavel2 = "Sepulveda" # Numero ao fim do nome
 # Em python esta atribuição pode ser feita de maneira mais simples e em apenas uma linha;
 # POR EXEMPLO -
 var1, var2, var3 = 1, 2, 3 # Esta forma de atribuição é mais simples mas prioriza a ordem a qual você que atribuir alguns valores.
+
+# As reproduzindo com um comando print o resultado sera o mesmo da sequencia
+print(var1) # Imprime 1
+print(var2) # Imprime 2
+print(var3) # Imprime 3
+
+# Podemos tambem, atribuir o mesmo valor a varias variaveis
+# var4 = "Antonio"
+# var5 = "Antonio"
+# var6 = "Antonio"
+
+# Porem a formas mais simples de se fazer isso, como esta:
+var4 = var5 = var6 = "Antonio"
+# Ao imprimir estas variaveis, o mesmo valor no caso "Antonio", será atribuido as 3
+print(var4) 
+print(var5) 
+print(var6)
+
+# DESCOMPACTAR - O ato de separar os trechos de um array em diferentes variaveis;
+# desta maneira:
+frutas = ['maça', 'banana', 'cereja'] # Coleção de frutas
+frutas1, frutas2, frutas3 = frutas # Cada variavel ira receber um dado de uma coleção
+
+print(frutas1)
+print(frutas2)
+print(frutas3)
+
+# Operador de concatenação
+palavra1 = "Mundo"
+
+# Juntando variaveis as frases
+print("Ola, " + palavra1)
+# Podemos somar variaveis dessa maneira
+nome = "Antonio"
+sobrenome = "Flavio"
+# A soma de nome e sobrenome deve resultar em nome completo
+nomeCompleto = nome + sobrenome
+print(nomeCompleto)
+
+# Este metodo de soma tambem serve para valores numericos
+valor1 = 1
+valor2 = 3
+
+soma = valor1 + valor2 # A soma dos valores resulta em 4
+print(soma) 
+
+# Obs.: Tentar somar uma string com um numero inteiro ou float, ira resultar em um erro.
+# Pra somar uma string e um numero, devemos utilizar o casting
+
+num = 20
+var = "K"
+juncao = str(num) + var
+print(juncao) # Esta forma ira resultar em calgo que não dara erro.
+
+# VARIAVEIS GLOBAIS E VARIAVEIS LOCAIS
+
+# A funções sao blocos de codigo que são chamados sempre que as funções são referenciadas
+# As funções são referenciadas por meio do comando def
+
+def myFunc():
+    print("Ola, mundo!!!!!!!!!!")
+# Pra executar a função deve executar-la
+myFunc() # Servem pra reaproveitamento de código;
+
+
+# Criando variaveis de escopo global:
+pal1 = "Antonio"
+# Criando a função:
+def meuNome():
+    print(pal1 + " Flavio")
+
+meuNome()
+print("Meu nome é: " + pal1)
+
+# As variaveis locais
+nick = "sunny"    # Esta é uma variavel global.
+
+def meuNick():
+    nick = "hallsey"  # Esta é uma variavel Local.
+    print(nick)
+
+meuNick()
+
+# Obs: Porque o nome da variavel mudou de sunny pra hallsey?
+'''
+    * As variaveis quando fora de uma função são denominadas de variaveis globais estas podem ser referenciadas de qualquer lugar do código, diferente das variaveis locais.
+    *As variaveis locais tem sua origem e uso dentro de suas respectivas funções, e as funções por mais que saibam da existencia de uma função global, dão prioridade as  funções locais, estas por sua vez só existem dentro de suas respectivas funções, similares as variaveis temporarias que criamos na    GML.
+    E se... eu printar a variavel do exemplo fora da função?
+'''
+print(nick) # Ira printar sunny;
+
+'''
+    Existe uma forma de criar uma variavel global dentro de uma funçao?
+    Sim, por meio do uso da palavra chave global.
+    As definimos como global, depois damos o valor delas.
+'''
+
+# Criação de variaveis globais dentro de uma função:
+def anoNovo():
+    global ano
+    ano = str(2024)
+    # Diga olá ao ano novo
+    print("Diga ola à " + ano)
+
+# chamando a função:
+anoNovo()
+# Chamando a variavel global dentro da função;
+print("ola " + ano)
+
+# A palavra chave Global tambem serve para trocar o valor da variavel
+
+varTeste = "Cubo Mágico"     # Esta é a variavel que deve ser alterada
+
+def exemplo():
+    # Modificando o valor da variavel de teste:
+    global varTeste
+    varTeste = "Enigma"
+    # Imprimindo o novo valor dado á variavel:
+    print("Resolvi o " + varTeste)
+
+exemplo()
+# Chamando a variavel que foi modificada:
+print("Resolvi o " + varTeste)
