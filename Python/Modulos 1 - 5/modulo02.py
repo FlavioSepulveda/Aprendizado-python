c34e26069cd2e1e333498056c72b657a06aca793
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-02 14:21:09 -0300

    Dia 2 - Inicio

--- Python/Modulos 1 - 5/modulo02.py
+++ Python/Modulos 1 - 5/modulo02.py
@@ -1,1 +1,20 @@
 # MODULO 02 - TIPOS DE DADOS
+'''
+    Anteriormente falei sobre a utilização e os tipos de casting, e oque cada método apresente.
+    Então recapitulando os tipos de dados que apresentei lá:
+'''
+
+# Tipos de dado Textual - str (String)
+# Tipos de dado numérico - int, float, complex(novo)
+
+# --------------------------------------------------
+
+# Os novos que vou apresentar são:
+# Tipos de dados de sequencia - list, tuple, range
+# Tipos de dados de mapeamento - dict
+# Tipos de dados de conjunto - set, frozenset
+# Tipos de dados boleanos - bool (Verdadeiro ou Falso/True ou False/1 ou 0)
+# Tipos binarios - Bytes, Bytearray, Memoryview
+
+# Para definir e exemplificar cada tipo de dado:
+
