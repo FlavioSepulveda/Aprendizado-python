9a39713a3c8f2450a743d3db60f740277dda114a
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-03 14:15:38 -0300

    Valores Booleanos - True or False

---
+++ Python/Modulos 1 - 5/Tipos de dados/modulo02-5.py
@@ -0,0 +1,7 @@
+# VALORES BOOLEANOS
+# Os valores booleanos são valores que se apresentam em 2 valores (1 ou 0) (True or False) (Verdadeiro ou Falso).
+
+True    # Verdadeiro
+False   # Falso
+
+# As expressões booleanas 
