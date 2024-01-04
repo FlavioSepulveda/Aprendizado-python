81e7f2abb8ad19b5d0124dfae32a2aafb121186c
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-03 23:53:38 -0300

    Operadores - Python

--- Python/Modulos 1 - 5/modulo03.py
+++ Python/Modulos 1 - 5/modulo03.py
@@ -1,1 +1,24 @@
 # MODULOS 03 - OPERADORES 
+
+# Os operadores aritimeticos em python são responsaveis por realozar operações matematicas.
+
+'''
+    São eles:
+    + - Operador de adição
+    - - Operador de subtração
+    * - Operador de multiplicação
+    / - Operador de Divisão
+    % - Operador de Modulo(Resto da divisão)
+    // - Operador de Arredondamento de divisão
+    ** - Operador de exponenciação
+'''
+
+# Operadores de atribuição
+
+# Operadores de comparação
+
+# Operadores lógicos
+
+# Operadores de identidade
+
+# Operadores de Associação
