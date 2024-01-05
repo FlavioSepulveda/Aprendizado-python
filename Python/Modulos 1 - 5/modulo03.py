bac2af3648bfb0548b6b8e252432cf5f66b30ae1
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-05 14:19:00 -0300

    Operadores de incremento

--- Python/Modulos 1 - 5/modulo03.py
+++ Python/Modulos 1 - 5/modulo03.py
@@ -16,18 +16,39 @@
 a = 5
 b = 2
 
-print(a + b) # Deve retornar 7
-print(a - b) # Deve retornar 3
-print(a * b) # Deve retornar 10
-print(a / b) # Deve retornar 2.5
-print(a % b) # Deve retornar 1
-print(a // b) # Deve retornar 2
-print(a ** b) # Deve retornar 25
+# print(a + b) # Deve retornar 7
+# print(a - b) # Deve retornar 3
+# print(a * b) # Deve retornar 10
+# print(a / b) # Deve retornar 2.5
+# print(a % b) # Deve retornar 1
+# print(a // b) # Deve retornar 2
+# print(a ** b) # Deve retornar 25
 
 # Operadores de atribuição
 # O Sinal de igual é um operador de atribuição simples:
 x = 5
+y = 5
+z = 5
+w = 5
+u = 5
+v = 5
+p = 5
 print(x)
+
+x += 5 # É o mesmo que x = x + 5
+print(x)
+y -= 1 # É o mesmo que y = y - 1
+print(y)
+z *= 2 # É o mesmo que z = z * 2
+print(z)
+w /= 2 # É o mesmo que w = w / 2
+print(w)
+u %= 2 # É o mesmo que u = u % 2
+print(u)
+v //= 2 # É o mesmo que v = v // 2
+print(v)
+p **= 2 # É o mesmo que p = p ** 2
+print(p) 
 
 # Dentre os operadores de atribuição é tambem conhecido como operadores de incrmento e decremento:
 
