81f7624414b16fa8411ab88aeadf3a8b08209969
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-05 21:37:13 -0300

    Inicio do conteudo de Listas

--- Python/Modulos 1 - 5/modulo04.py
+++ Python/Modulos 1 - 5/modulo04.py
@@ -1,1 +1,17 @@
 # MODULO 04 - Coleções - LISTAS
+
+# Listas são dados do tipo coleção de dados, mas ainda sendo objetos deste tipo, elas obedecem algumas regras basicas para existir.
+
+# REGRA NUMERO 1 - As listas são criadas dentro de colchetes.
+# Ex.: lista = ['exemplo1', 'exemplo2', 'exemplo3']
+# REGRA NUMERO 2 - Cada item da lista é separado por virgula, e entre aspas simples quando se for uma string.
+
+'''
+    Observações -
+    * Nas listas os itens são: Mutaveis e é permitido valores duplicados.
+    * As listas são indexadas ou seja os itens dentro dela obedecem a contagem que começa a partir do 0.
+    * Apos a criação da lista a ordem dos itens dentro do indice não pode ser alterada.
+'''
+
+primeira_lista = ['Bolo', 'Lasanha', 'Cafe']
+print(primeira_lista)
