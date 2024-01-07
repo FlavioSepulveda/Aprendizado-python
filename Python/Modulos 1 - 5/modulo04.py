831a036cb14fd7bee2dc596fe04d78a0156aac94
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-06 22:06:35 -0300

    Acessando indices da lista

--- Python/Modulos 1 - 5/modulo04.py
+++ Python/Modulos 1 - 5/modulo04.py
@@ -34,8 +34,42 @@ terceira_lista = list(('Sherlock Holmes', 'H.P Lovecraft', 'Edgar Alan Poe'))
 print(terceira_lista)
 
 # Alterando itens da lista
+'''
+    Os indices das listas começam a partir do 0, assim como num array.
+    tendo em vista isso podemos chamar o item de dentro da lista por meio de seu indice.
+'''
+# Chamando um indice
+
+quarta_lista = ['Flavio', 'Ricardo', 'Gabriel']
+
+# Caso eu queira ter acesso ao primeiro nome da lista 'Flavio', devo chamar pelo indice 0 da seguinte forma:
+print(quarta_lista[0]) # Ira imprimir 'Flavio' no terminal.
+
+'''
+    *OBS.: Em python possuimos uma funcionalidade que permite a utilização de valores negativos, pois ao chamar por -1, por exemplo ele ira retornar o ultimo item da lista, que no caso do exemplo é 'Gabriel'. 
+    Esta funcionalidade percorre a lista a partir do -1 Referenciando o ultimo item, de trás pra frente.
+'''
+
+print(quarta_lista[-1]) # Imprime gabriel no terminal.
+
+# Criando um quinta lista de objetos domesticos:
+quinta_lista = ['Colher', 'Tesoura', 'Prato', 'Geladeira', 'Ventilador', 'Relógio de parede', 'Livro']
+
+# Com essa funcionalidade podemos acessar intervalos dentro da lista 
+print(quinta_lista[2:5]) # acessando o indice 2 a 5 (Prato ate ventilador), o indice 5 é excluso
+print(quinta_lista[:4]) # Podemos indicar desta maneira caso precise iniciar o indice 0
+print(quinta_lista[3:]) # Podemos indicar que queremos ate o final da lista
+# indexação negativa:
+print(quinta_lista[-4:-1]) # Ele ira acessar de Geladeira a relogio relogio de parede, sempre excluindo o ultimo intem.
+
+# LEMBRETE - Para checar se um item esta ou não dentro de uma lista, utilizamos o 'in'
+if 'Colher' in quinta_lista:
+    print('Sim, Colher esta na lista.') # Lembre-se, O python verifica letras maiusculas de minusculas
+else:
+    print('Não, colher não esta na lista.')
 
 # Adicionando itens na lista
+
 
 # Remover itens da lista
 
@@ -49,4 +83,3 @@ print(terceira_lista)
 
 # Juntando listas
 
-# 
