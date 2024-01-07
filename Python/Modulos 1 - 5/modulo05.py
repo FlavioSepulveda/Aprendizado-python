82da8d5b0414b9034e8d375f6f9cbbdaeb3a3d26
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-07 13:25:33 -0300

    Iniciando em Tuplas

--- Python/Modulos 1 - 5/modulo05.py
+++ Python/Modulos 1 - 5/modulo05.py
@@ -1,1 +1,13 @@
 # MODULO 05 - Coleções - TUPLAS
+
+# Tuplas python - Regras
+
+# Acessando itens de uma tupla
+
+# Atualizar tuplas
+
+# Descompactando tuplas
+
+# Loops atravez de uma tupla
+
+# Juntar Tuplas 
