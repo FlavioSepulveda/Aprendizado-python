600938e81d444cf8a564f02fbe05593d9020795b
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-08 19:54:17 -0300

    Organização antes de Estudar

--- Python/Modulos 6 - 10/modulo06.py
+++ Python/Modulos 6 - 10/modulo06.py
@@ -1,1 +1,13 @@
 # MODULO 06 - Coleções - SET
+
+# Set Python -
+
+# Acessando os itens do set
+
+# Adcionando itens do set
+
+# Remover itens do set
+
+# Loop Atraves de um set
+
+# Juntando coleções do tipo set 
