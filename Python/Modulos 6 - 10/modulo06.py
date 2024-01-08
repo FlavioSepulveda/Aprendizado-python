5984a424c37f5e7dea32da4fd256064202f83ee0
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-08 20:31:49 -0300

    Tipo Set - 01

--- Python/Modulos 6 - 10/modulo06.py
+++ Python/Modulos 6 - 10/modulo06.py
@@ -1,6 +1,24 @@
 # MODULO 06 - Coleções - SET
 
 # Set Python -
+'''
+    Os dados de tipo set são usadas pra armazenar dados assim com as dos tipos anteriores.
+    Os Dados do tipo set não são Indexados e não são ordenados. 
+    Os Dados do tipo set são escritos entre chaves.
+'''
+set1 = {'Flávio', 'Marcos', 'Samuel', 'Pedro'}
+
+print(set1) # A ordem sempre muda ao chamar o mesmo item.
+# Os dados dentro do set podem ser adcionados mas nao podem ser modificados ou duplicados durante sua criação.
+# Ele ignora os itens repetidos
+print(len(set1)) # Podemos ter o tamanho do set
+s2 = {1,2,3,4} # Podemos ter dados do tipo inteiro.
+s2 = {True, False, True, False} # Podemos ter dados do tipo booleano.
+s2 = {1,"ola",3.0,True} # Podemos ter dados Mesclados.
+
+# Podemos usar o cosntrutor do tipo set para criar um set;
+set2 = set(('Carro', 'Moto', 'Caminhão', 'Barco')) # Dois parenteses lembre-se
+print(set2)
 
 # Acessando os itens do set
 
