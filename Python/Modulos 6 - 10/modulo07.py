547f751c56c0f30e5c92d6470e23d4215c2deeff
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-09 16:26:38 -0300

    Dicionarios 1

--- Python/Modulos 6 - 10/modulo07.py
+++ Python/Modulos 6 - 10/modulo07.py
@@ -1,1 +1,27 @@
 # MODULO 07 - Coleções - DICTIONARY
+
+# Dicionarios em python
+'''
+    # Os dicionarios assim como os dados do tipo set se encontram dentro de chaves, porem apresentando 2 informações sendo elas 'A chave' : 'O valor', sendo o valor separado de sua chave por dois pontos e de outras chaves com virgula.
+    # Cada Key (Chave), Representa um valor dentro do dicionario.
+    # Os Dicionarios não permitem itens duplicados.
+'''
+# Seguindo com um exemplo onde pegamos um personagem de anime pra apresentar ele.
+luffy = {
+    'Anime' : 'One Piece',
+    'Comida favorita' : 'Carne',
+    'Nacionalidade' : 'Brasileiro'
+}
+# Os itens dentro de um dicionario podem ser de varios tipos, inclusive podem de numeros inteiros e float, inclusive eles podem ser utilizados como chaves. 
+
+# Acesso a itens dentro dos dicionarios
+
+# Adição de itens dentro dos dicionarios
+
+# Remoção de itens dentro dos dicionarios
+
+# Percorrendo os dicionarios com loops
+
+# Copiando dicionarios
+
+# Aninhando dicionarios
