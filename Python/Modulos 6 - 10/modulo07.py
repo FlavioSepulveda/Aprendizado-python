eb4b92b47045f1af22fec221fd00379c7d2b5376
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-09 16:59:02 -0300

    Buscando e adcionando itens dentro de um dicionario

--- Python/Modulos 6 - 10/modulo07.py
+++ Python/Modulos 6 - 10/modulo07.py
@@ -15,6 +15,47 @@ luffy = {
 # Os itens dentro de um dicionario podem ser de varios tipos, inclusive podem de numeros inteiros e float, inclusive eles podem ser utilizados como chaves. 
 
 # Acesso a itens dentro dos dicionarios
+zoro = {
+    'Anime' : "One Piece",
+    'Onde esta' : 'Perdido',
+    'Comida Favorita': 'Pinga'
+}
+
+
+# Podemos pegar o valor de uma chave usando uma variavel para quardar um valor.
+x = zoro['Anime']
+print(x)
+# Ira reproduzir o valor daquela chave.
+# Outra forma é com o metodo get
+print(zoro.get('Onde esta'))
+# Podemos criar uma variavel e atribuir o get dentro dela.
+z = zoro.get('Onde esta')
+
+# O Método keys retorna todas as chaves na forma de uma lista
+chaves = zoro.keys()
+print(chaves)
+# Qualquer alteração ira refletir na keys
+# Exemplo:
+zoro['Quantidade de espadas'] = 3
+print(chaves)
+
+# Método values
+chaves = zoro.values()
+print(chaves)
+
+zoro['Altura'] = 1.90
+print(chaves)
+
+# Metodo itens - Retorna uma tupla
+s = zoro.items()
+print(s)
+zoro['Altura'] = 1.95
+print(s)
+# Esse metodo mostra todas as alterações;
+# Para checar a existencia de determinado item dentro do dicionario, usamos o metodo in.
+# EXEMPLO -
+if 'Comida Favorita' in zoro:
+    print('Sim, ele gosta de cachaça.')
 
 # Adição de itens dentro dos dicionarios
 
