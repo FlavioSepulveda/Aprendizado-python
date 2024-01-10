db9865bb6b9caab818e5e50c420f2524eea5082e
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-10 17:29:27 -0300

    Operadores lógicos

--- Python/Modulos 6 - 10/modulo08.py
+++ Python/Modulos 6 - 10/modulo08.py
@@ -1,50 +1,55 @@
-# MODULO 08 - Estruturas condicionais
+# Módulo 08 - Estruturas Condicionais
 
-# If  e Else -
+# Estruturas Condicionais:
 '''
-    As estruturas condicionais são esttruturas que vão executar um codigo dependendo do resultado de uma expressão.
-    # Os operadores são:
-    # Igual a: a == b
-    # Difefrente de: a != b
-    # Menor que: a < b
-    # Menor ou igual a: a <= b
-    # Maior que: a > b
-    # Maior ou igual a: a >= b
+    Executam código baseado no resultado de uma expressão.
+    Operadores: 
+    ==, !=, <, <=, >, >=
 '''
+
+# Variáveis de exemplo:
 a = 33
 b = 200
 
-# Estrutura condicional;
-# Se B for maior que a:
+# If e Else:
 if b > a:
-    print('B é maior que A.') # Deve sempre haver uma identação de tabulação que é um espaço de 4 caracteres
+    print('B é maior que A.')
     print('Outra instrução no if.')
-elif a == b: # Similar ao else if
-    print('Sim, A é igual a B.')
-elif a + 150 != b: # Similar ao else if
-    print('A é diferente de B.') # o programa não executa essa por parar na primeira execução verdadeira.
-else: # Executa se nenhuma for verdadeira
+elif a == b:
+    print('A é igual a B.')
+elif a + 150 != b:
+    print('A é diferente de B.')
+else:
     print('Todas as verificações anteriores são falsas.')
-# podemos tter quantos elifs forem nescessarios no programa e durante sua execução.
 print('Fim da Execução.')
 
-# Short Hand If Else -
-# As condicionais são feitas em apenas uma linhas da seguinte forma
-
+# Short Hand If Else:
 a = 200
 b = 50
 
-# Fazemos assim para o if:
-if a > b: print('A é maior que b.')
+# If em uma linha:
+if a > b: print('A é maior que B.')
 
-# Para o else assim:
+# Else em uma linha:
 print("A") if a < b else print("B")
 
-# operadores ternarios:
+# Operadores Ternários:
 print(" a ") if a < b else print(" = ") if a == b else print(" b ")
 
-# Operadores lógicos -
+# Operadores Lógicos - and e or:
+a = 200
+b = 33
+c = 500
 
-# Estruturas condicionais aninhadas -
+# Este operador retorna verdadeiro quando todas as condições são verdade.
+if a > b and c > a:
+    print('A é maior que B.')
+    print('E C é maior que A.')
 
-# A declaração de passagem -
+# Este operador retorna verdadeiro quando uma das condições são verdade.
+if a < b or c > a:
+    print("Pelo menos uma é verdadeira.")
+
+# Estruturas Condicionais Aninhadas:
+# Declaração 'pass':
+# Evita erros de sintaxe em estruturas ainda não implementadas.
