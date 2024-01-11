0a2b16d28399ade2c714580fa2ea070687357258
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-11 12:06:21 -0300

    Loop for

--- Python/Modulos 6 - 10/modulo09.py
+++ Python/Modulos 6 - 10/modulo09.py
@@ -13,3 +13,24 @@ else:
     print('O valor de i não é mais menor que 8. ')
 
 print('Fim da execução do loop.')
+
+# Criando um loop for:
+# O loop for serve para interar nnuma sequencia;
+
+nome = ['Gabriel', 'Danny', 'Arthur']
+
+# Criando o loop for para printar os itens da sequencia for:
+for x in nome:
+    print(x)
+
+print('Fim da execução.')
+
+# Ele tambem inteira numa cadeia de caracteres:
+# Por exemplo -
+
+for x in 'Ola mundo':
+    print(x)
+
+# Podemos  usar um else neste estilo de loop taambem:
+else:
+    print('Fim da execução.')
