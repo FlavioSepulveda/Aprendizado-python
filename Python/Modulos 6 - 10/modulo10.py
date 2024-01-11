dac553f382511f356bf9c2740933a4faabdb63fb
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-11 17:42:15 -0300

    Inicio de Funções

--- Python/Modulos 6 - 10/modulo10.py
+++ Python/Modulos 6 - 10/modulo10.py
@@ -1,1 +1,20 @@
 # MODULO 10 - Funções Python
+# Funções são blocos de codigos chamados sempre que as funções são chamadas
+
+def myFunc():
+    # Esta função escreve a mensagem abaixo na tela.
+    print('Esta é minha primeira função.')
+
+myFunc()
+# Os parametros indicam oque a função recebe.
+def myFunc2(nome):
+    print('Ola', nome,' !')
+
+myFunc2("Flavio")
+
+# As funções retornam valores:
+def myFunc3(x):
+    return x * 5
+
+resultado = myFunc3(2)
+print(resultado)
