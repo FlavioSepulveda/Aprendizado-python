da3638b8dc0b443210e452d590e82fbb5193b3fc
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-11 10:41:32 -0300

    Loop While

--- Python/Modulos 6 - 10/modulo09.py
+++ Python/Modulos 6 - 10/modulo09.py
@@ -1,1 +1,15 @@
 # MODULO 09 - Estrutura de repetição (loops)
+# Loop While: o loop while é um loop que roda infinitamente enquanto a condicional para sua execução for verdadeira;
+
+# Prieiro para montar este tipo de loop precisamos de uma variavel de controle que neste caso é 'i'.
+i = 0
+
+# A estrutura do loop while se baseaia na palavra reservada 'while';
+while i < 8:
+    print(i) #  Imprime todos os numeros de 0 a 7, (8 numeros no total).
+    i += 1
+# Podemos utilizar instruçõies else dentro desses loops.
+else:
+    print('O valor de i não é mais menor que 8. ')
+
+print('Fim da execução do loop.')
