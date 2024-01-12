ee92fcacf5b48b42d1d6e2b17a3b6d8189344455
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-12 17:22:17 -0300

    Classes 2

--- Python/Modulos 11 - 15/modulo13.py
+++ Python/Modulos 11 - 15/modulo13.py
@@ -11,3 +11,24 @@ print(MyClass) # Retorna o tipo do objeto
 p1 = MyClass()
 p1.x # Acessando o valor dentro da classe.
 print(p1.x) # vai imprimir o x de dentro da classe.
+
+# A função interna init que acompanha as classes em sua criação;
+# A função init starta junto com a classe servindo para delimitar certas caracteristicas dentro da classe.
+
+'''
+    Self - Vem do ingles, e significa "A mim mesmo", dentro das classes seria algo do tipo:
+    "A mim mesmo darei o atributo idade."
+'''
+    # As funções dentro da classe sempre iniciam com Self.
+class MinhaClasse:
+
+    def __init__(self, nome_user, idade_user) -> None:
+        self.nome = nome_user
+        self.idade = idade_user
+
+
+# Classes são como seções em livros de receitas.
+client = MinhaClasse("Antonio", 21)
+
+print(client.nome, client.idade)
+    
