ccdf06a9ad07acdb80a6bebe3424e282c8d91ae9
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-12 16:49:33 -0300

    Iniciando (POO)
    Não entendi porra nenhuma.

--- Python/Modulos 11 - 15/modulo13.py
+++ Python/Modulos 11 - 15/modulo13.py
@@ -1,1 +1,13 @@
 # MODULOS 13 - Programação Orientada a objetos (POO)
+
+'''
+    Classes são construtores de objetos.
+'''
+class MyClass:
+    x = 5
+
+print(MyClass) # Retorna o tipo do objeto
+# Criando um objeto derivado desta classe:
+p1 = MyClass()
+p1.x # Acessando o valor dentro da classe.
+print(p1.x) # vai imprimir o x de dentro da classe.
