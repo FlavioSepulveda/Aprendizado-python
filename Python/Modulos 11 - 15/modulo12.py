f6d78a6b51f46802c8357f2bf24f9833351c94a5
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-11 22:20:04 -0300

    Finalizando dia 13

--- Python/Modulos 11 - 15/modulo12.py
+++ Python/Modulos 11 - 15/modulo12.py
@@ -15,3 +15,17 @@ print(cars)
 
 # Para saber o tamanho do array:
 print(len(cars))
+
+# Trabalhando listas como arrays:
+comidas = ['Uva', 'Goiaba', 'Morango']
+
+for x in comidas:
+    print(x)
+
+# Adicionando itens.
+# Usamos o metodo append:
+    comidas.append('Abacaxi')
+# Em algumas linguagens não se é permitido modificar arrays, ja no python é pelo simples fato dele ser uma lista;
+    
+comidas.pop(1) # Vai remover o valor do indice 1.
+comidas.remove('Uva') # remove um item pelo nome e não pelo indice.
