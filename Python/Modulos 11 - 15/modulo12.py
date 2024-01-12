269e7dcdd10034a4c2dbe909a7db7ac2bde58e97
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-11 22:10:12 -0300

    Arrays como Coleções de dados

--- Python/Modulos 11 - 15/modulo12.py
+++ Python/Modulos 11 - 15/modulo12.py
@@ -1,1 +1,17 @@
 # MODULOS 12 - Arrays e Coleções de Dados
+
+# No python não temos suporte para arrays, então usamos listas em seu lugar:
+cars = ['Ford', 'Volvo', 'BMW']
+print(cars)
+
+# Para acessar um item numa matriz usamos um indice
+x = cars[0]
+print(x)
+# Ele ira imprimir 'Ford'
+
+# Para trocar o valor funciona assim;
+cars[0] = 'Toyota'
+print(cars)
+
+# Para saber o tamanho do array:
+print(len(cars))
