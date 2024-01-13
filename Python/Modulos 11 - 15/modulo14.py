42f8d3ae2bfb8cbd5575258f0e748e0cc0c52b9e
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-13 12:34:06 -0300

    Iterator 1

--- Python/Modulos 11 - 15/modulo14.py
+++ Python/Modulos 11 - 15/modulo14.py
@@ -1,1 +1,28 @@
 # MODULOS 14 - Interadores Python
+
+# Os interadores são - Objetos que contem numeros contados de valores.
+# Inter & Next
+# As listas, tuplas e sets - Sao interadores.
+
+myTuple = ('Flavio', 'Caio', 'Carlos')
+myInt = iter(myTuple)
+
+# O metodo next pega sempre o proximo codigo de um interavel.
+print(next(myInt)) 
+print(next(myInt))
+print(next(myInt))
+# As strings tbm são interaveis, elas possuem um interador tbm.
+
+fruta = 'Banana'
+myInt2 = iter(fruta)
+
+print(next(myInt2))
+print(next(myInt2))
+print(next(myInt2))
+print(next(myInt2))
+print(next(myInt2))
+print(next(myInt2))
+
+# Um método for cria algo semelhante a um iterator 
+for x in myTuple:
+    print(x)
