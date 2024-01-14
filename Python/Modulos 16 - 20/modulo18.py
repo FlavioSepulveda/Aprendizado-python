7bf0a18db4d290de41db18a700cd9fab0c235eaa
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-14 18:11:18 -0300

    Min & Max

--- Python/Modulos 16 - 20/modulo18.py
+++ Python/Modulos 16 - 20/modulo18.py
@@ -1,1 +1,15 @@
 # MODULO 18 - Matemática em Python
+
+# O conjunto de funções matematicas do python
+# Funções min e max
+# Função min retorna o valor minimo
+x = min(5, 10, 52, 89,100)
+print(x)
+
+# Funciona pra listas tbm
+a = [7,15,25,32,4,18]
+y = min(a)
+print(y)
+# Função max retorna o valor maximo
+print(max(x))
+print(max(y))
