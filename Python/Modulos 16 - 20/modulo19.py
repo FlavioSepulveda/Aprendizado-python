29675e2dea9feee1f6a6672d70a5c8e0c1130cf9
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-15 13:10:48 -0300

    Conversão de Python para JSON

--- Python/Modulos 16 - 20/modulo19.py
+++ Python/Modulos 16 - 20/modulo19.py
@@ -1,1 +1,13 @@
 # MODULO 19 - Linguagem JSON e Phython
+# Trabalho com JSON no python
+
+import json
+# Convertendo um objeto json para python por meio do metodo Loads()
+x = '{"Nome": "Flavio", "Idade": 21, "Cidade": "Teresina"}'
+
+y = json.loads(x)
+print(y)
+print(type(y))
+print(y["Nome"])
+print(y["Idade"])
+print(y["Cidade"])
