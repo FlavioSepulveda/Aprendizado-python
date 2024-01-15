0213b6c78ad84fdfd1d774073312e89be263a20f
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-15 14:14:08 -0300

    Conversão de python para json

--- Python/Modulos 16 - 20/modulo19.py
+++ Python/Modulos 16 - 20/modulo19.py
@@ -7,7 +7,28 @@ x = '{"Nome": "Flavio", "Idade": 21, "Cidade": "Teresina"}'
 
 y = json.loads(x)
 print(y)
-print(type(y))
-print(y["Nome"])
-print(y["Idade"])
-print(y["Cidade"])
+# print(type(y))
+# print(y["Nome"])
+# print(y["Idade"])
+# print(y["Cidade"])
+# Convertendo python pra json por meio do metodo dumps
+x1 = {
+      "Nome": "Flavio", 
+      "Idade": 21, 
+      "Cidade": "Teresina"
+      }
+# Realizando a conversão
+y1 = json.dumps(x1)
+print(y1)
+print(type(x1)) # Python
+print(type(y1)) # JSON
+# Exemplos de objetos python que retornam dados json
+print(json.dumps({"Nome": "Flavio", "Idade": 21, "Cidade": "Teresina"}))
+print(json.dumps(['Maçã', 'Banana']))
+print(json.dumps(('Maçã', 'Banana')))
+print(json.dumps('Ola, mundo'))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
