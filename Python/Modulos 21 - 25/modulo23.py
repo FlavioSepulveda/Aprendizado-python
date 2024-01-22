3c85d3a43277032e156c98e8beaeb9633c14b039
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-22 16:40:12 -0300

    Tratamento de arquivos

--- Python/Modulos 21 - 25/modulo23.py
+++ Python/Modulos 21 - 25/modulo23.py
@@ -16,3 +16,16 @@
     "t" - Texto - valor padrão modo texto;
     "b" - Binário - Modo binario (por exemplo, imagens);
 '''
+
+# 
+f = open('C:\\Users\\flavi\\OneDrive\\Desktop\\Py\\Aprendizado-python\\Python\\Modulos 21 - 25\\teste.txt', 'r', encoding="UTF-8")
+# Para ver o conteudo do arquivo funciona assim
+# print(f.read())
+
+# Métodos de leitura
+
+# print(f.readline(2))
+for x in f:
+    print(x)
+    # fechando o arquivo (boas praticas)
+f.close()   
