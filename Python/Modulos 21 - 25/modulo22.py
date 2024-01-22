01cbe8127d20b144d26670ff39d615d725da32b5
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-22 00:12:00 -0300

    Aninhando exceções

--- Python/Modulos 21 - 25/modulo22.py
+++ Python/Modulos 21 - 25/modulo22.py
@@ -11,3 +11,18 @@ except:
 
 print('Continuando o programa.')
 # O bloco try trata qualquer erro de código
+# Podemos executar multiplas exceções definidas para tipos de erros diferentes
+y = "ola mundo"
+try:
+    print(y)
+except NameError:
+    print('Variavel inexistente.')
+except:
+    print('Significo exceção')
+else:
+    print('Tudo certo por aqui.')
+
+
+# Os except são executados para erros em especifico, mas podemos usar um else pra executar algo fora do try.
+
+print('Continuando o programa.')
