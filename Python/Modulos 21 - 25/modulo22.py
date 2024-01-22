34671c287bc4a8853900bfd212cba6b6976bd2bd
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-22 00:26:03 -0300

    Lançando erros.

--- Python/Modulos 21 - 25/modulo22.py
+++ Python/Modulos 21 - 25/modulo22.py
@@ -20,11 +20,21 @@ except NameError:
 except:
     print('Significo exceção')
 else:
+    # Os except são executados para erros em especifico, mas podemos usar um else pra executar algo fora do try.
     print('Tudo certo por aqui.')
 finally:
+    # o finally executa dequalquer forma dentro do bloco de codigos
     print('O try except foi finalizado.')
-# o finally executa dequalquer forma dentro do bloco de codigos
 
-# Os except são executados para erros em especifico, mas podemos usar um else pra executar algo fora do try.
+# Gerando uma exceção
 
-print('Continuando o programa.')
+x = 5
+# criando uma exceção/lançando uma exceção - métoddo raise
+if x < 0:
+    raise Exception('Numero invalido.')
+# Exceções definidas
+y1 = "Ola mundo"
+if not type(y1) is int:
+    raise TypeError("Somente numeros")
+
+print('Fim do programa.')
