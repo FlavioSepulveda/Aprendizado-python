e062240316a68fb8c759c2c9b33be44634c35b6f
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-22 00:06:00 -0300

    Tratamento de erros

--- Python/Modulos 21 - 25/modulo22.py
+++ Python/Modulos 21 - 25/modulo22.py
@@ -1,1 +1,13 @@
 # MODULO 22 - Manipulação de erros ou exceção
+# 
+# Tratamentos de exceção
+# x = 'ola mundo.'
+
+# A variavel não estando definida deve gerar um erro e qualquer codigo abaixo não ira continuar.
+try:
+    print(x)
+except:
+    print('Significo exceção')
+
+print('Continuando o programa.')
+# O bloco try trata qualquer erro de código
