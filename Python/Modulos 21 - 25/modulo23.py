f07819480018ce5b7a294c8dd8825769b9168180
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-22 11:27:54 -0300

    Tratamento de arquivos em python

--- Python/Modulos 21 - 25/modulo23.py
+++ Python/Modulos 21 - 25/modulo23.py
@@ -1,1 +1,18 @@
 # MODULO 23 - Manipular arquivo (Criação, Leitura e Escrita)
+# 
+# open("Nome do Arquivo", "Modo")
+
+# MODOS -
+'''
+    "r" - Ler - é o valor padrao;
+    "a" - Anexar - abre um arquivo para anexar o conteudo, ou o cria se ele não existir;
+    "w" - Gravar - abre um arquivo para gravação, cria o arquivo se ele não existir;
+    "x" - Cirar - Cria o arquivo especificado, retorna um erro se o arquivo não existir;
+'''
+
+# TIPOS -
+'''
+    Podemos especificar o modo que o arquivo sera tratado da seguinte forma.
+    "t" - Texto - valor padrão modo texto;
+    "b" - Binário - Modo binario (por exemplo, imagens);
+'''
