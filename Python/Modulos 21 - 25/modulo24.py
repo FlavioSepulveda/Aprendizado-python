23d2e9f132e28d9ddbd2006345cc670bd6e65ece
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-23 15:23:20 -0300

    Criando Tabelas

--- Python/Modulos 21 - 25/modulo24.py
+++ Python/Modulos 21 - 25/modulo24.py
@@ -10,7 +10,9 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="teste123", 
-    password="batata123"
+    password="batata123",
+    # Criando um parametro que referencia a data base
+    database='my_database'
 )
 
 # print(mydb)
@@ -18,7 +20,18 @@ mydb = mysql.connector.connect(
 # Precisamos criar um objeto cursor.
 m_cursor = mydb.cursor()
 # m_cursor.execute('CREATE DATABASE my_database') 
-m_cursor.execute("SHOW DATABASES")
+# m_cursor.execute("SHOW DATABASES")
 
+# for x in m_cursor:
+#     print(x)
+    
+# Criando tabelas:
+# sql = """CREATE TABLE 
+#             pessoas (nome VARCHAR(225), 
+#             idade INT(2))"""
+
+# m_cursor.execute(sql)
+# Acessando as tabelas de dados:
+m_cursor.execute('SHOW TABLES')
 for x in m_cursor:
     print(x)
