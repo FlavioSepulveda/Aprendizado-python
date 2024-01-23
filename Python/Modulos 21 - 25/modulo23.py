93dfbacf7bbf5354a89857f5446a1fedf1f7eebb
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-01-22 23:56:40 -0300

    Tratamento de arquivos 3

--- Python/Modulos 21 - 25/modulo23.py
+++ Python/Modulos 21 - 25/modulo23.py
@@ -18,14 +18,31 @@
 '''
 
 # 
-f = open('C:\\Users\\flavi\\OneDrive\\Desktop\\Py\\Aprendizado-python\\Python\\Modulos 21 - 25\\teste.txt', 'r', encoding="UTF-8")
-# Para ver o conteudo do arquivo funciona assim
+# f = open('C:\\Users\\flavi\\OneDrive\\Desktop\\Py\\Aprendizado-python\\Python\\Modulos 21 - 25\\teste.txt', 'r', encoding="UTF-8")
+# # Para ver o conteudo do arquivo funciona assim
+# # print(f.read())
+
+# # Métodos de leitura
+
+# # print(f.readline(2))
+# for x in f:
+#     print(x)
+#     # fechando o arquivo (boas praticas)
+# f.close()   
+
+# criando um arquivo em python
+x = open("Ola, mundo.txt", "r", encoding="UTF-8")
+print(x.read())
+# quando o arquivo ja existe:
+#  "a"
+f = open("Ola, mundo.txt", "a")
+f.write("\nAdicionando conteudo ao arquivo.")
+f.close()
+
+# f=open("Ola, mundo.txt", "r")
 # print(f.read())
-
-# Métodos de leitura
-
-# print(f.readline(2))
-for x in f:
-    print(x)
-    # fechando o arquivo (boas praticas)
-f.close()   
+# f.close()
+# "w" - Reescrevendo o arquivo
+f=open("Ola, mundo.txt", "w")
+f.write("Reescrevendo o conteudo.")
+f.close()
