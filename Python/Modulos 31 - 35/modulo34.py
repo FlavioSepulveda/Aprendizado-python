ebc35aad501602741a0445cdab65df7db9ed0250
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-03-10 00:52:39 -0300

    plot lib

--- Python/Modulos 31 - 35/modulo34.py
+++ Python/Modulos 31 - 35/modulo34.py
@@ -1,1 +1,12 @@
 # MODULO 34 - Python Matplotlib
+import matplotlib.pyplot as plt
+
+# print(mp.__version__)
+
+# Plotagem de grafico
+# Passmaos cordenadas e ele desenha o grafico
+xpoints = [0,6, 8]
+ypoints = [0, 15, 10]
+plt.plot(xpoints, ypoints)
+
+plt.show()
