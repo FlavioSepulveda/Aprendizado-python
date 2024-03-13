79b01769c2dc8e573e644d918364073d04a9c6f4
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-03-13 15:54:52 -0300

    Grafico de dispersão

---
+++ Python/Modulos 31 - 35/MODULO  24/Grafico dispers.pyw
@@ -0,0 +1,16 @@
+# Grafico de dispersão
+import matplotlib.pyplot as plt
+
+# Dia 1 - Idade e Velocidade de 13 carros
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+plt.scatter(x, y)
+
+# Dia 2 - Idade e Velocidade de 15 carros
+x = [2,2,8,1,15,8,12,9,7,3,11,4,7,14,12]
+y = [100,105,84,105,90,99,90,95,94,100,79,112,91,80,85]
+plt.scatter(x, y)
+
+# Função scatter -
+
+plt.show() 
--- Python/Modulos 31 - 35/modulo34.py
+++ Python/Modulos 31 - 35/modulo34.py
@@ -6,8 +6,40 @@ import matplotlib.pyplot as plt
 # Plotagem de grafico
 # Passmaos cordenadas iniciais no zero e apos ele os pontos onde o grafico sofre deformidades -
 
+# Plot  1 -
 xpoints = [0, 6, 8, 10, 13]
 ypoints = [0, 15, 10, 12, 13]
+plt.subplot(2, 3, 1)  # Linhas, Colunas, Indices
+plt.title('Vendas')
+plt.plot(xpoints, ypoints, ls='dashed', color='r')
+# Montando um segundo plot
+# Plot 2 -
+xpoints = [0, 6, 8, 10, 13]
+ypoints = [3, 14, 0, 8, 13]
+plt.subplot(2, 3, 2)
+plt.title('Lucro')
+plt.plot(xpoints, ypoints, ls='dashed', color='r')
+# Plot 3 -
+xpoints = [0, 6, 8, 10, 13]
+ypoints = [3, 14, 0, 8, 13]
+plt.subplot(2, 3, 3)
+plt.title('Prejuizos')
+plt.plot(xpoints, ypoints, ls='dashed', color='r')
+# # Plot 4 -
+# xpoints = [0, 6, 8, 10, 13]
+# ypoints = [3, 14, 0, 8, 13]
+# plt.subplot(2,3,4)
+# plt.plot(xpoints, ypoints, ls='dashed', color='r')
+# # Plot 5 -
+# xpoints = [0, 6, 8, 10, 13]
+# ypoints = [3, 14, 0, 8, 13]
+# plt.subplot(2,3,5)
+# plt.plot(xpoints, ypoints, ls='dashed', color='r')
+# # Plot 5 -
+# xpoints = [0, 6, 8, 10, 13]
+# ypoints = [3, 14, 0, 8, 13]
+# plt.subplot(2,3,6)
+# plt.plot(xpoints, ypoints, ls='dashed', color='r')
 
 # # Colocamos as listas nas variaveis de pontos no eixo x e y para encontrar um resultado de forma dinamica -
 # # plt.plot(xpoints)
@@ -67,19 +99,23 @@ plt.plot(xpoints, ypoints, ls='dashed', color='r')
 # linewidth ou lw - Para definir expessura
 # linestyle ou ls - Para definir o estilo de linha
 
-font1 = {'family':'serif', 'color':'blue', 'size':20} # Configurando a fonte
-font2 = {'family':'serif', 'color':'blue', 'size':10}
+font1 = {'family': 'serif', 'color': 'blue',
+         'size': 15}  # Configurando a fonte
+font2 = {'family': 'serif', 'color': 'blue', 'size': 10}
 
 # Rotulos e titulos do Matplotlib
-plt.title('Modulo pyplot', fontdict=font1, loc='left') # Define o titulo que Identifica o grafico, ele possue 3 valores
+# plt.title('Modulo pyplot', fontdict=font1, loc='left') # Define o titulo que Identifica o grafico, ele possue 3 valores
+plt.suptitle('Minha Loja', fontdict=font1)
 # Left, Right e Center
-plt.xlabel('Valor x (Exemplo 1)',fontdict=font2) # Define a etiqueta que identifica o eixo x
-plt.ylabel('Valor y (Exemplo 2)',fontdict=font2) # Define a etiqueta que identifica o eixo y
+# Define a etiqueta que identifica o eixo x
+plt.xlabel('Valor x (Exemplo 1)', fontdict=font2)
+# Define a etiqueta que identifica o eixo y
+plt.ylabel('Valor y (Exemplo 2)', fontdict=font2)
 
 # Adcionando linhas de grade -
 
-# plt.grid()  
+# plt.grid()
 # plt.grid(axis='x')
-plt.grid(axis='y', color='r')  # Definindo a linha de destaque 'Axis'
+# plt.grid(axis='y', color='r')  # Definindo a linha de destaque 'Axis'
 
 plt.show()
