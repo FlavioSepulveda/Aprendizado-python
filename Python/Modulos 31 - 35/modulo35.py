9ed54e0f205acd381f4ce92d2313289706639e60
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-03-15 11:01:42 -0300

    Numpy

--- Python/Modulos 31 - 35/modulo35.py
+++ Python/Modulos 31 - 35/modulo35.py
@@ -1,1 +1,14 @@
 # MODULO 35 - Módulo  Python NumPy
+import numpy as np
+
+# O numpy é uma biblioteca que visa o trabalho com arrays no python sendo melhor que as listas que visam ter um objetivo parecido com um array mas sendo mais lento, ja o numpy oferece uma velocidade 50x maior com o objeto 'nparray'
+
+# Ele serve para trabalhos com algebra linear em python por isso o nome NumPy
+
+# arr = np.array([1,2,3,4,5])
+# print(arr)
+
+# print(np.__version__)
+
+# Criação de matrizes -
+
